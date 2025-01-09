97cc0fb49681b865232f4f9916286e34e2386ec6
Vishwa Joshi <107062706+Vishwa-J@users.noreply.github.com>
2025-01-09 18:44:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,16 @@ import pyjokes
 
 app=Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=['GET'])
 def home():
     joke=pyjokes.get_joke()
     return f'<h2>{joke}</h2>'
 
-@app.route("/MultipleJokes")
+@app.route("/MultipleJokes",methods=['GET'])
 def jokes():
     jokes="<br><br>".join(pyjokes.get_jokes())
     return f'<h3>{jokes}</h3>'
 
 if __name__=="__main__":
-    app.run(debug=False)
+    app.run()
     
